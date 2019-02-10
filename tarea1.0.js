51f7b6b6b8685df3abbc7a35e4669857412f58b7
Coffe759c <47493933+Coffe759c@users.noreply.github.com>
2019-02-10 02:18:29 -0600

    Create tarea1.0.js

---
+++ tarea1.0.js
@@ -0,0 +1,11 @@
+let num1= 5
+let num2= 2
+
+let suma = num1+num2 
+let resta = num1-num2
+let mult = num1* num2
+let sumcua = (num1*num1)+(num2*num2)
+console.log("resultado de la suma: " +suma)
+console.log("resultado de la resta: "+resta)
+console.log("resultado de la multiplicacion: " +mult)
+console.log("resultado de la suma de los cuadrados de ambos numeros: "+sumcua)
